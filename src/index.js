e193176858714ee1597c1b27dfc426863b7739d9
Juanjo Simon <jjotasaimon@gmail.com>
2023-02-06 18:13:28 +0700

    Dropdown menu create and fully working

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import css from "./styles.css";
+
+// Get all the dropdowns
+const dropdowns = document.querySelectorAll(".dropdown");
+
+// Loop through all the dropdown elements
+dropdowns.forEach((dropdown) => {
+  // Get inner elements on each dropdown
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelectorAll(".menu li");
+  const selected = dropdown.querySelector(".selected");
+
+  //Click event on select element
+  select.addEventListener("click", () => {
+    select.classList.toggle("selected-clicked");
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
+  });
+
+  //Loop through option elements
+  options.forEach((option) => {
+    //Add a click event to the option element
+    option.addEventListener("click", () => {
+      console.log(option);
+      selected.innerText = option.innerText;
+      select.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+      //Remove active class from all option elements
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      //Add active class to clicked option
+      option.classList.add("active");
+    });
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "source-map",
+  stats: {
+    errorDetails: true,
+    children: true,
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/inline",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ToDo List",
+      template: "src/index.html",
+    }),
+  ],
+};
